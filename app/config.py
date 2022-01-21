659c5d3755521f3afaec8996b1dc18b293dd0d2d
synalice <nikita.nikita.krasnov@gmail.com>
2022-01-21 17:42:26 +0300

    fix: change config.py path to .env
    For some reason app/.env as a path to .env file caused dotenv_values to give <class 'NoneType'> data type.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 from dotenv import dotenv_values
 
 
-config = dotenv_values("app/.env")
+# Why doesn't it work as ("app/.env")?!
+config = dotenv_values(".env")
 
 # Create .env file in the same directory and add BOT_API_TOKEN variable there to use bot
 BOT_API_TOKEN = config.get("BOT_API_TOKEN")

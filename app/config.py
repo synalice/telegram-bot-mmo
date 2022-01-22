f52820b4170e1e61903d1354a104cc4a7ebb99c7
Artem Burenin <61312225+burenotti@users.noreply.github.com>
2022-01-22 16:20:03 +0300

    Добавил системные переменные окружения в конфиг
    dotenv_values(".env") берет значения исключительно из файла, игнорируя переменные окружения. Это может быть неудобно при развертывании в Docker, так как передать файл при запуске контейнера нельзя, а переменную среды -- можно, поэтому код был изменен так, что объявленная переменная среды закроет собой переменную из .env файла.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
+import os
 from dotenv import dotenv_values
 
 
-config = dotenv_values(".env")
+config = {**dotenv_values(".env"), **os.environ}
 
 # Create .env file in the same directory and add BOT_API_TOKEN variable there to use bot
 BOT_API_TOKEN = config.get("BOT_API_TOKEN")

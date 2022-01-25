3debd55b57e01d57732d76c8e098add24f9f2bb5
synalice <nikita.nikita.krasnov@gmail.com>
2022-01-25 17:37:20 +0300

    feat: add id indexing in a table

--- app/db/db_schemas.py
+++ app/db/db_schemas.py
@@ -6,6 +6,8 @@ class TelegramUserProfile(Base):
 	telegram_id = sa.Column(sa.Integer, primary_key=True)
 	telegram_firstname = sa.Column(sa.String, nullable=False)
 	telegram_lastname = sa.Column(sa.String)
+	# Not sure if simply adding Index right here will be enough to do something. I hope it will.
+	sa.Index(telegram_id, unique=True)  # Not sure if unique=True is needed since telegram_id is pk.
 
 
 # class UserProfile(Base):

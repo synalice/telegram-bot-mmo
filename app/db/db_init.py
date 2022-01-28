20404c58588e22a57bdff80bbe61c8a60e37956e
synalice <nikita.nikita.krasnov@gmail.com>
2022-01-28 21:57:45 +0300

    <dev>

--- app/db/db_init.py
+++ app/db/db_init.py
@@ -1,6 +1,4 @@
 import sqlalchemy.ext.asyncio as sa_async
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import sessionmaker
 
 from app.config import (
     DB_NAME,
@@ -11,24 +9,14 @@ from app.config import (
     DB_PORT,
     DB_DATABASE,
 )
-from app.db.db_schemas import Base
 
 
-engine = None
+engine = sa_async.create_async_engine(
+    f"{DB_NAME}{DB_DRIVER}://{DB_LOGIN}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}",
+    echo=False,
+)
 
-
-async def async_main():
-    global engine
-    engine = sa_async.create_async_engine(
-        f"{DB_NAME}{DB_DRIVER}://{DB_LOGIN}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}",
-        echo=False,
-    )
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def create_async_session():
-    async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
-    return async_session
+# with engine.begin() as conn:
+#     conn.run_sync(Base.metadata.drop_all)
+#     conn.run_sync(Base.metadata.create_all)
+# ?????????
--- app/db/db_register.py
+++ app/db/db_register.py
@@ -1,12 +1,14 @@
 import sqlalchemy as sa
-from app.db.db_init import create_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-
+from app.db.db_init import engine
 from app.db.db_schemas import TelegramUserProfile
 
 
 async def register_new_user(user_id, first_name, last_name):
-    async with (await create_async_session()) as session:
+    async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
+    async with async_session() as session:
         async with session.begin():
             querry_user = sa.select(TelegramUserProfile).where(
                 TelegramUserProfile.telegram_id == user_id
@@ -14,9 +16,14 @@ async def register_new_user(user_id, first_name, last_name):
             user = await session.execute(querry_user)
             await session.commit()
     if not user:
-        user = TelegramUserProfile(
-            telegram_id=user_id,
-            telegram_firstname=first_name,
-            telegram_lastname=last_name,
-        )
+        async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
+        async with async_session() as session:
+            async with session.begin():
+                user = TelegramUserProfile(
+                    telegram_id=user_id,
+                    telegram_firstname=first_name,
+                    telegram_lastname=last_name,
+                )
+                await session.add(user)
+                await session.commit()
     return user

3852f620ac613714a2f18c59858ea7db959475f1
synalice <nikita.nikita.krasnov@gmail.com>
2022-02-05 20:18:39 +0300

    feat(db): create tables if none exist

--- app/db/db_init.py
+++ app/db/db_init.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import sqlalchemy.ext.asyncio as sa_async
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -10,11 +12,25 @@ from app.config import (
     DB_PORT,
     DB_DATABASE,
 )
+from app.db.db_schemas import Base
 
 
 engine = sa_async.create_async_engine(
     f"{DB_NAME_N_DRIVER}://{DB_LOGIN}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}",
     echo=True,
 )
+
+
+async def init_db():
+    """
+    This function runs synchronous metadata.create_all to create all the tables describes in db_schemas
+    Thanks to @burenotti for helping with writing this.
+    """
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init_db())
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)

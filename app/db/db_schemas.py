f08c1a5fc62a3f5b754abcc1b59a4eab0eef71fc
synalice <nikita.nikita.krasnov@gmail.com>
2022-02-08 17:38:14 +0300

    refactor(DATABASE): rename 'telegram_user_profile' table

--- app/db/db_schemas.py
+++ app/db/db_schemas.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -6,8 +7,16 @@ Base = declarative_base()
 
 
 class TelegramUserProfile(Base):
-    __tablename__ = 'telegram_user_profile'
+    __tablename__ = 'telegram_profile'
     telegram_id = sa.Column(sa.Integer, primary_key=True)
     telegram_firstname = sa.Column(sa.String, nullable=False)
     telegram_lastname = sa.Column(sa.String)
     sa.Index(telegram_id, unique=True)
+#     children = relationship("Child", back_populates="parent")
+#
+# class PlayerUserProfile(Base):
+#     __tablename__ = 'player_user_profile'
+#     player_id = sa.Column(sa.Integer, primary_key=True)
+#     player_firstname = sa.Column(sa.String, nullable=False)
+#     player_lvl = sa.Integer
+#     sa.Index(telegram_id, unique=True)
